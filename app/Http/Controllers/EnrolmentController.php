218cfbc2ba1dd0d8ce41b3fc4b26473158db34b2
root <yorozuya3@protonmail.com>
2022-02-07 11:24:48 +0000

    Create models, migrations and controllers for enrolments

---
+++ app/Http/Controllers/EnrolmentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\enrolment;
+use Illuminate\Http\Request;
+
+class EnrolmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\enrolment  $enrolment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(enrolment $enrolment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\enrolment  $enrolment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(enrolment $enrolment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\enrolment  $enrolment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, enrolment $enrolment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\enrolment  $enrolment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(enrolment $enrolment)
+    {
+        //
+    }
+}
