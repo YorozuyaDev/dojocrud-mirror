233a5e39e2a61e44e43eaa185251b9c61190e8fc
root <yorozuya3@protonmail.com>
2022-02-07 11:17:40 +0000

    Create laravel app

---
+++ app/Http/Controllers/LessonsMediaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LessonsMedia;
+use Illuminate\Http\Request;
+
+class LessonsMediaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LessonsMedia  $lessonsMedia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LessonsMedia $lessonsMedia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LessonsMedia  $lessonsMedia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LessonsMedia $lessonsMedia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LessonsMedia  $lessonsMedia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LessonsMedia $lessonsMedia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LessonsMedia  $lessonsMedia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LessonsMedia $lessonsMedia)
+    {
+        //
+    }
+}
